96fa1ddcb460be0ad343a4ae8513c71d720f42da
tony <sumonroytony@gmail.com>
2022-10-29 21:41:12 +0600

    created a project with cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\sumon\Desktop\workspace\Learing-Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\sumon\Desktop\workspace\Learing-Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\sumon\Desktop\workspace\Learing-Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\sumon\Desktop\workspace\Learing-Rust\hello_cargo\src\main.rs
